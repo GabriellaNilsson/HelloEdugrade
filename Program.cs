87919ad5c78e302a471130a7ea1c9b655cd89998
gabri <gabri@DESKTOP-CPPV5ML>
2023-09-06 15:50:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloEdugrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            var CurrentDate = DateTime.Now;
+            //DateTime CurrentDate = DateTime.Now;
+            Console.WriteLine($"Hej, {name}, på {CurrentDate}");
+            //Console.WriteLine($"Hej, {name}, på {CurrentDate:d}");
+            // $ = formatera. När det används, som i ovan, kan man kalla på innehållet inom "måsvingar" som variabel. 
+        }
+        
+    }
+}
